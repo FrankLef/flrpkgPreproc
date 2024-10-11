d287aefcee0b5e1b04c0f7731fbf56100ce10324
FrankLef <flefebvre01@hotmail.com>
2024-10-11 16:01:49 -0400

    ddict_transf_dat: ym

--- tests/testthat/test-ddict_transf_dat.R
+++ tests/testthat/test-ddict_transf_dat.R
@@ -16,3 +16,22 @@ test_that("ddict_transf_dat_wk", {
   target <- c(names(df), c("cont_date_wk", "clean_date_wk"))
   expect_identical(names(out), target)
 })
+
+test_that("ddict_transf_dat_ym", {
+  # testthat::skip("debug")
+  df <- data_clean("df")
+  # cat("\n", "df", "\n")
+  # str(df)
+  ddict_data <- data_clean("ddict")
+  # cat("\n", "ddict_data", "\n")
+  # str(ddict_data)
+  ddict <- DDict(ddict_data)
+  # cat("\n", "ddict", "\n")
+  # print(utils::head(ddict@data))
+
+  out <- ddict_transf_dat_ym(ddict, data = df, table_nm = "data1")
+  # str(out)
+
+  target <- c(names(df), c("cont_date_ym", "clean_date_ym"))
+  expect_identical(names(out), target)
+})
