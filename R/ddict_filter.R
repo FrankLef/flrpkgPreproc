cb16ab91f955f9f10578321c6ca9e2e0b0b76427
FrankLef <flefebvre01@hotmail.com>
2024-12-14 08:07:02 -0500

    fixx(filter)

--- R/ddict_filter.R
+++ R/ddict_filter.R
@@ -1,9 +1,8 @@
 ddict_filter <- S7::new_generic(
   "DDict",
   dispatch_args = "object",
-  fun = function(
-      object, ..., table_nm = NULL,
-      role_rgx = NULL, process_rgx = NULL, rule_rgx = NULL, pull_var = NULL) {
+  fun = function(object, ..., table_nm = NULL,
+                 role_rgx = NULL, process_rgx = NULL, rule_rgx = NULL, pull_var = NULL) {
     checkmate::assert_string(table_nm, min.chars = 1, null.ok = TRUE)
     checkmate::assert_string(role_rgx, na.ok = TRUE, null.ok = TRUE)
     checkmate::assert_string(process_rgx, na.ok = TRUE, null.ok = TRUE)
